2242148050526ba09962e837a2eb77cb1c49ac72
ryanakann <ryanakann@gmail.com>
2020-04-18 22:38:32 -0400

    GWAVITYYYYY

--- Axes/Assets/Scripts/PlayerInput.cs
+++ Axes/Assets/Scripts/PlayerInput.cs
@@ -1,24 +1,77 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
 
-[RequireComponent (typeof (Player))]
+[RequireComponent(typeof(CharacterController2D))]
 public class PlayerInput : MonoBehaviour {
+    private CharacterController2D controller;
 
-	Player player;
+    private BufferedButton jump;
+    private BufferedButton crouch;
 
-	void Start () {
-		player = GetComponent<Player> ();
-	}
+    private void Start () {
+        jump = new BufferedButton("Jump");
+        crouch = new BufferedButton("Crouch");
 
-	void Update () {
-		Vector2 directionalInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
-		player.SetDirectionalInput (directionalInput);
+        controller = GetComponent<CharacterController2D>();
+    }
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			player.OnJumpInputDown ();
-		}
-		if (Input.GetKeyUp (KeyCode.Space)) {
-			player.OnJumpInputUp ();
-		}
-	}
+    private void Update () {
+        jump.UpdateButton();
+        crouch.UpdateButton();
+
+        controller.Move(Input.GetAxis("Horizontal"), crouch.Down(), jump.Down(), jump.Stay());
+    }
+}
+
+public class BufferedButton {
+    private string buttonName;
+    private int bufferSize;
+    private int bufferDown;
+    private int bufferStay;
+    private int bufferUp;
+
+    public BufferedButton (string buttonName, int bufferSize = 4) {
+        this.buttonName = buttonName;
+        this.bufferSize = bufferSize;
+    }
+
+    public void UpdateButton () {
+        if (Input.GetButtonDown(buttonName)) {
+            bufferDown = bufferSize;
+        } else {
+            if (bufferDown > 0) {
+                bufferDown--;
+            }
+        }
+
+        if (Input.GetButton(buttonName)) {
+            bufferStay = bufferSize;
+        } else {
+            if (bufferStay > 0) {
+                bufferStay--;
+            }
+        }
+
+        if (Input.GetButtonUp(buttonName)) {
+            bufferUp = bufferSize;
+        } else {
+            if (bufferUp > 0) {
+                bufferUp--;
+            }
+        }
+    }
+
+    public bool Down () {
+        return bufferDown > 0;
+    }
+
+    public bool Stay () {
+        return bufferStay > 0;
+    }
+
+    public bool Up () {
+        return bufferUp > 0;
+    }
 }
