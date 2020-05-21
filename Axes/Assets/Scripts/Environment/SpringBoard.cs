448efaf8c252337b820562dcab8c139ff9cc5462
ryanakann <ryanakann@gmail.com>
2020-05-21 01:44:43 -0400

    Springs and buttons

--- Axes/Assets/Scripts/Environment/SpringBoard.cs
+++ Axes/Assets/Scripts/Environment/SpringBoard.cs
@@ -2,58 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpringBoard : MonoBehaviour
-{
+public class SpringBoard : MonoBehaviour {
+    SpringJoint2D springJoint;
+    Transform top, mid, bot;
 
-    SpringJoint2D spring_joint;
-    LineRenderer lr;
-    Transform base_board, base_point, rope_point;
+    public float springThreshold = 0.5f;
+    public float springForce = 5f;
+    public float launchCD = 0.25f;
+    [SerializeField]
+    private float curLaunchCD;
 
-    float spring_threshold = 0.5f, spring_force = 5f;
+    float initialSpringLength;
+    float currentSpringLength;
+    Vector3 midsectionInitialScale;
+    Vector3 springInitialPosition;
 
-    List<Rigidbody2D> carried_objects = new List<Rigidbody2D>();
+    Collider2D[] carriedObjects;
+    BoxCollider2D launchArea;
+    Rigidbody2D objReference;
 
     // Start is called before the first frame update
-    void Awake()
-    {
-        spring_joint = GetComponent<SpringJoint2D>();
-        lr = GetComponent<LineRenderer>();
-        lr.useWorldSpace = true;
-        base_board = transform.parent.FindDeepChild("Base");
-        rope_point = transform.FindDeepChild("RopePoint");
-        base_point = transform.parent.FindDeepChild("BasePoint");
+    void Awake() {
+        top = transform.Find("Spring_Top");
+        mid = transform.Find("Spring_Mid");
+        bot = transform.Find("Spring_Bot");
+        springJoint = top.GetComponent<SpringJoint2D>();
+        initialSpringLength = (top.position-bot.position).magnitude;
+        midsectionInitialScale = mid.localScale;
+        springInitialPosition = top.position;
+        launchArea = top.GetComponent<BoxCollider2D>();
+        launchArea.enabled = false;
+        curLaunchCD = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        lr.SetPositions(new Vector3[] { rope_point.position, base_point.position});
+    void Update() {
+        //Scale the mid section springy bit based on how compressed the spring is
+        currentSpringLength = (top.position-bot.position).magnitude;
+        mid.localScale = new Vector3(midsectionInitialScale.x, 
+                                     midsectionInitialScale.y * Mathf.InverseLerp(0, initialSpringLength, currentSpringLength), 
+                                     midsectionInitialScale.z);
 
-        if (Vector2.Dot(spring_joint.reactionForce, transform.position - base_board.position) > spring_threshold)
-        {
-            foreach (Rigidbody2D r in carried_objects)
-            {
-                print(r);
-                r.AddForce(transform.up * spring_force, ForceMode2D.Impulse);
-            }
+        if (Vector2.Dot(springJoint.reactionForce, top.position - springInitialPosition) > springThreshold) {
+            if (curLaunchCD <= 0f) {
+                print("KABOOM");
+                curLaunchCD = launchCD;
+                carriedObjects = Physics2D.OverlapBoxAll((Vector2)top.transform.position + launchArea.offset, launchArea.size, 0f);
+                foreach (Collider2D obj in carriedObjects) {
+                    if (null != (objReference = obj.GetComponent<Rigidbody2D>()) && !obj.name.Contains("Spring")) {
+                        objReference.velocity = Vector2.zero;
+                        objReference.AddForce(transform.up * springForce, ForceMode2D.Impulse);
+                    }
+                }
+            }    
+        } else {
+            print($"Force: {Vector2.Dot(springJoint.reactionForce, top.position - springInitialPosition)} - Threshold: {springThreshold}");
+        }
+        
+        //Prevent double launches
+        if (curLaunchCD < 0f) {
+            curLaunchCD = 0f;
+            springJoint.frequency = 1f;
+        } else if (curLaunchCD > 0f) {
+            curLaunchCD -= Time.deltaTime;  
+            springJoint.frequency = 10f;
+        } else {
+            springJoint.frequency = 1f;
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Rigidbody2D coll_rb = collision.GetComponent<Rigidbody2D>();
-        if (coll_rb && !carried_objects.Contains(coll_rb)) 
-        {
-            carried_objects.Add(coll_rb);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        Rigidbody2D coll_rb = collision.GetComponent<Rigidbody2D>();
-        if (coll_rb && carried_objects.Contains(coll_rb))
-        {
-            carried_objects.Remove(coll_rb);
-        }
+    void OnDrawGizmos () {
+        if (!Application.isPlaying) return;
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawCube((Vector2)top.transform.position + launchArea.offset, launchArea.size);
     }
 }
