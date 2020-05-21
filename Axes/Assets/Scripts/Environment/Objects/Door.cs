37ced8001056f6d66463653696550bcb858ba8c1
ryanakann <ryanakann@gmail.com>
2020-05-21 19:27:50 -0400

    Door and button

--- Axes/Assets/Scripts/Environment/Objects/Door.cs
+++ Axes/Assets/Scripts/Environment/Objects/Door.cs
@@ -6,13 +6,15 @@ using UnityEngine.Events;
 public class Door : MonoBehaviour {
     [Header("Transition")]
     public float transitionDuration = 1f;
-    [Tooltip("Normalized to (0-1) on X and Y axes.")]
+    [Tooltip("Normalize to (0-1) on X and Y axes.")]
     public AnimationCurve tween;    
 
     [Header("Events")]
+    public int lockCount = 1;
+    private int currentUnlockCount = 0;
+    public LockMode lockMode;
     public UnityEvent OnDoorOpen;
     public UnityEvent OnDoorClose;
-
 
     private Transform door;
 
@@ -23,52 +25,87 @@ public class Door : MonoBehaviour {
     private Vector3 openPosition;
 
     private void Awake () {
+        // OpenRequirements.
         door = transform.Find("Door_Key");
         open = false;
         transitioning = false;
 
         closedPosition = door.localPosition;
         openPosition = door.localPosition + Vector3.up * -2f * door.localPosition.y;
+
+        currentUnlockCount = 0;
+    }
+
+    private void Update () {
+        if (transitioning) return;
+        if (open && IsLocked()) Close();
+    }
+
+    public bool IsOpen () {
+        return open;
+    }
+
+    private bool IsLocked () {
+        bool unlocked;
+        switch(lockMode) {
+            case LockMode.and:
+                unlocked = currentUnlockCount == lockCount;
+                break;
+            case LockMode.nand:
+                unlocked = currentUnlockCount < lockCount;
+                break;
+            case LockMode.nor:
+                unlocked = currentUnlockCount == 0;
+                break;
+            case LockMode.or:
+                unlocked = currentUnlockCount > 0;
+                break;
+            default:
+                unlocked = false;
+            break;
+        }
+        return !unlocked;
     }
 
     public void Open () {
-        if (open || transitioning) return;
+        if (open) return;
+        currentUnlockCount++;
+        if (IsLocked()) return;
+        open = true;
+        if (transitioning) return;
         transitioning = true;
         OnDoorOpen.Invoke();
-        StartCoroutine(OpenCR());
-    }
-
-    private IEnumerator OpenCR () {
-        float t = 0f;
-
-        while (t < 1f) {
-            // print($"t: {t} - tween: {tween.Evaluate(t)}");
-            door.localPosition = Vector3.Lerp(closedPosition, openPosition, tween.Evaluate(t));
-            t += Time.deltaTime / transitionDuration;
-            yield return new WaitForEndOfFrame();
-        }
-        door.localPosition = openPosition;
-        open = true;
-        transitioning = false;
+        StartCoroutine(TransitionCR());
     }
 
     public void Close () {
-        if (!open || transitioning) return;
+        if (!open) return;
+        currentUnlockCount--;
+        if (!IsLocked()) return;
+        open = false;
+        if (transitioning) return;
         transitioning = true;
         OnDoorClose.Invoke();
-        StartCoroutine(CloseCR());
+        StartCoroutine(TransitionCR());
     }
 
-    private IEnumerator CloseCR () {
-        float t = 0f;
-
-        while (t < 1f) {
-            door.localPosition = Vector3.Lerp(openPosition, closedPosition, tween.Evaluate(t));
-            t += Time.deltaTime / transitionDuration;
+    private IEnumerator TransitionCR () {
+        float t = open ? 0f : 1f;
+        bool finished = false;
+        while (!finished) {
+            door.localPosition = Vector3.Lerp(closedPosition, openPosition, tween.Evaluate(t));
+            t += Time.deltaTime / transitionDuration * (open ? 1 : -1);
+            finished = (open ? t >= 1f : t <= 0f);
             yield return new WaitForEndOfFrame();
         }
-        door.localPosition = closedPosition;
-        open = false;
+        door.localPosition = open ? openPosition : closedPosition;
         transitioning = false;
+    }
+
+    public enum LockMode {
+        and,
+        or,
+        nand,
+        nor,
     }
 }
