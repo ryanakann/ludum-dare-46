0e7ea23ad46585694d2ba3a20370a6c245e688b8
ryanakann <ryanakann@gmail.com>
2020-06-13 22:45:16 -0400

    Scaling level

--- Axes/Assets/Scripts/CameraController.cs
+++ Axes/Assets/Scripts/CameraController.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 using Cinemachine;
 
 [RequireComponent(typeof(CinemachineConfiner))]
-public class CameraConfinerAutoSetup : MonoBehaviour {
+public class CameraController : MonoBehaviour {
     private CinemachineConfiner confiner;
+    private CinemachineVirtualCamera cam;
     private new PolygonCollider2D collider;
+    
+    public float size = 7f;
+
     private void OnValidate () {
         Setup();
+        
     }
 
     private void Start () {
         Setup();
     }
 
+    private void Update () {
+        cam.m_Lens.OrthographicSize = size;
+    }
+
     private void Setup () {
         if (confiner == null) {
             confiner = GetComponent<CinemachineConfiner>();
         }
+
+        if (cam == null) {
+            cam = GetComponent<CinemachineVirtualCamera>();
+        }
+        cam.m_Lens.OrthographicSize = size;
 
         if (collider == null) {
             foreach (var col in FindObjectsOfType<PolygonCollider2D>()) {
