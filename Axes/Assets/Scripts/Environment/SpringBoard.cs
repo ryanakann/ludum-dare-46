0b7b8b8e0de4b761adefafa4e5b0ed21b338849d
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-08-12 21:49:00 -0400

    stop saying KABOOM lmao

--- Axes/Assets/Scripts/Environment/SpringBoard.cs
+++ Axes/Assets/Scripts/Environment/SpringBoard.cs
@@ -27,7 +27,7 @@ public class SpringBoard : MonoBehaviour {
         mid = transform.Find("Spring_Mid");
         bot = transform.Find("Spring_Bot");
         springJoint = top.GetComponent<SpringJoint2D>();
-        initialSpringLength = (top.position-bot.position).magnitude;
+        initialSpringLength = (top.position - bot.position).magnitude;
         midsectionInitialScale = mid.localScale;
         springInitialPosition = top.position;
         launchArea = top.GetComponent<BoxCollider2D>();
@@ -37,14 +37,13 @@ public class SpringBoard : MonoBehaviour {
 
     void Update() {
         //Scale the mid section springy bit based on how compressed the spring is
-        currentSpringLength = (top.position-bot.position).magnitude;
-        mid.localScale = new Vector3(midsectionInitialScale.x, 
-                                     midsectionInitialScale.y * Mathf.InverseLerp(0, initialSpringLength, currentSpringLength), 
+        currentSpringLength = (top.position - bot.position).magnitude;
+        mid.localScale = new Vector3(midsectionInitialScale.x,
+                                     midsectionInitialScale.y * Mathf.InverseLerp(0, initialSpringLength, currentSpringLength),
                                      midsectionInitialScale.z);
 
         if (Vector2.Dot(springJoint.reactionForce, top.position - springInitialPosition) > springThreshold) {
             if (curLaunchCD <= 0f) {
-                print("KABOOM");
                 curLaunchCD = launchCD;
                 carriedObjects = Physics2D.OverlapBoxAll((Vector2)top.transform.position + launchArea.offset, launchArea.size, 0f);
                 foreach (Collider2D obj in carriedObjects) {
@@ -53,24 +52,24 @@ public class SpringBoard : MonoBehaviour {
                         objReference.AddForce(transform.up * springForce, ForceMode2D.Impulse);
                     }
                 }
-            }    
+            }
         } else {
             // print($"Force: {Vector2.Dot(springJoint.reactionForce, top.position - springInitialPosition)} - Threshold: {springThreshold}");
         }
-        
+
         //Prevent double launches
         if (curLaunchCD < 0f) {
             curLaunchCD = 0f;
             springJoint.frequency = 1f;
         } else if (curLaunchCD > 0f) {
-            curLaunchCD -= Time.deltaTime;  
+            curLaunchCD -= Time.deltaTime;
             springJoint.frequency = 10f;
         } else {
             springJoint.frequency = 1f;
         }
     }
 
-    void OnDrawGizmos () {
+    void OnDrawGizmos() {
         if (!Application.isPlaying) return;
         Gizmos.color = Color.cyan;
         Gizmos.DrawCube((Vector2)top.transform.position + launchArea.offset, launchArea.size);
